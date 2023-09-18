62542b9ef37c9c91847716c900e32dc7aa444474
Cesar Arenas <c.eduardoarenas@gmail.com>
2023-09-18 12:57:45 -0600

    Funciones completas

--- practica2/sorting/main.c
+++ practica2/sorting/main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "orden.h"
 
-/*
-Implementacion de algoritmos de ordenamiento y verificacion de tiempos de ejecucion
-1.-Burbuja
-2.-Seleccion
-3.-Insercion
-4.-Counting
-5.-Bucket
-6.-Timsort
-7.-Quick
-8.-Gnome
+// Función para generar un arreglo de números aleatorios de tamaño n
+void generarArregloAleatorio(int arr[], int n) {
+    srand(time(NULL)); // Inicializar la semilla para números aleatorios
 
-*/
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 1000; // Genera números aleatorios entre 0 y 999 (puedes ajustar el rango)
+    }
+}
+// Función para medir el tiempo de ejecución de una función de ordenamiento
+void medirTiempo(void (*ordenamiento)(int[], int), int arr[], int longitud, const char* nombre) {
+    clock_t inicio, fin;
+    double tiempo;
+
+    inicio = clock();
+    ordenamiento(arr, longitud);
+    fin = clock();
+
+    tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+    printf("Tiempo de ejecucion de %s: %lf segundos\n", nombre, tiempo);
+}
+
 int main() {
-    int arreglo[] = {5, 2, 3, 1, 4};
+    int n = 10; // Tamaño del arreglo aleatorio
+
+    int arreglo[n];
+
+    // Generar el arreglo aleatorio
+    generarArregloAleatorio(arreglo, n);
     int longitud = sizeof(arreglo) / sizeof(arreglo[0]);
 
-    counting(arreglo, longitud);
+    // Medir el tiempo de ejecución del algoritmo Gnome Sort
+    medirTiempo(bucketSort, arreglo, longitud, "bucketsort");
 
     printf("Arreglo ordenado: ");
     for (int i = 0; i < longitud; i++) {
--- practica2/sorting/orden.c
+++ practica2/sorting/orden.c
@@ -1,5 +1,11 @@
+//
+// Created by 52552 on 18/09/2023.
+//
 #include "orden.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MIN_MERGE 32
 
 void intercambiar(int *a, int *b) {
     int temporal = *a;
@@ -70,3 +76,169 @@ void counting(int arreglo[], int longitud) {
         }
     }
 }
+
+void bucketSort(int arreglo[], int longitud) {
+    if (longitud <= 0) return;
+
+    // Encontramos el valor máximo en el arreglo
+    int max = arreglo[0];
+    for (int i = 1; i < longitud; i++) {
+        if (arreglo[i] > max) {
+            max = arreglo[i];
+        }
+    }
+
+    // Creamos los cubos (buckets)
+    int *buckets = (int *)malloc(sizeof(int) * (max + 1));
+    if (buckets == NULL) {
+        printf("Error de asignación de memoria\n");
+        return;
+    }
+
+    // Inicializamos todos los cubos a 0
+    for (int i = 0; i <= max; i++) {
+        buckets[i] = 0;
+    }
+
+    // Colocamos los elementos en los cubos
+    for (int i = 0; i < longitud; i++) {
+        buckets[arreglo[i]]++;
+    }
+
+    // Recorremos los cubos y colocamos los elementos en el arreglo original
+    int index = 0;
+    for (int i = 0; i <= max; i++) {
+        while (buckets[i] > 0) {
+            arreglo[index++] = i;
+            buckets[i]--;
+        }
+    }
+
+    // Liberamos la memoria de los cubos
+    free(buckets);
+}
+
+// TIMSORT
+int comparar(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void merge(int arreglo[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++) {
+        L[i] = arreglo[l + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = arreglo[m + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arreglo[k] = L[i];
+            i++;
+        } else {
+            arreglo[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arreglo[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arreglo[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Función de ordenamiento por Timsort
+void timsort(int arreglo[], int longitud) {
+    if (longitud <= 0) return;
+
+    // Ordenar pequeños subarreglos usando insertion sort
+    for (int i = 0; i < longitud; i += MIN_MERGE) {
+        int left = i;
+        int right = (i + MIN_MERGE - 1 < longitud - 1) ? (i + MIN_MERGE - 1) : (longitud - 1);
+        qsort(&arreglo[left], right - left + 1, sizeof(int), comparar);
+    }
+
+    // Fusionar subarreglos en uno solo
+    for (int size = MIN_MERGE; size < longitud; size = 2 * size) {
+        for (int left = 0; left < longitud; left += 2 * size) {
+            int mid = left + size - 1;
+            int right = (left + 2 * size - 1 < longitud - 1) ? (left + 2 * size - 1) : (longitud - 1);
+            if (mid < right) {
+                merge(arreglo, left, mid, right);
+            }
+        }
+    }
+}
+
+// Función de partición para el algoritmo Quick Sort
+int particion(int arreglo[], int low, int high) {
+    int pivot = arreglo[high]; // Tomamos el último elemento como pivote
+    int i = (low - 1); // El índice del elemento más pequeño
+
+    for (int j = low; j <= high - 1; j++) {
+        // Si el elemento actual es menor o igual al pivote
+        if (arreglo[j] <= pivot) {
+            i++; // Incrementamos el índice del elemento más pequeño
+            // Intercambiamos arr[i] y arr[j]
+            int temp = arreglo[i];
+            arreglo[i] = arreglo[j];
+            arreglo[j] = temp;
+        }
+    }
+
+    // Intercambiamos arr[i+1] y arr[high] (el pivote)
+    int temp = arreglo[i + 1];
+    arreglo[i + 1] = arreglo[high];
+    arreglo[high] = temp;
+
+    return (i + 1);
+}
+
+// Función recursiva para el algoritmo Quick Sort
+void quickSort(int arreglo[], int low, int high) {
+    if (low < high) {
+        // Encontramos el índice del elemento pivote
+        int pi = particion(arreglo, low, high);
+
+        // Ordenamos los elementos antes y después de la partición
+        quickSort(arreglo, low, pi - 1);
+        quickSort(arreglo, pi + 1, high);
+    }
+}
+
+void gnomeSort(int arreglo[], int longitud) {
+    int index = 0;
+
+    while (index < longitud) {
+        if (index == 0) {
+            index++;
+        }
+        if (arreglo[index] >= arreglo[index - 1]) {
+            index++;
+        } else {
+            // Intercambiar elementos si están en el orden incorrecto
+            int temp = arreglo[index];
+            arreglo[index] = arreglo[index - 1];
+            arreglo[index - 1] = temp;
+            index--;
+        }
+    }
+}
+
