ca75b3e4ce708c172e175d98238b08513f476171
Cesar Arenas <c.eduardoarenas@gmail.com>
2023-10-06 11:30:10 -0600

    Panchita tontis

--- practica3/busquedapp.h
+++ practica3/busquedapp.h
@@ -1,6 +1,10 @@
 #ifndef BUSQUEDAPP_H_INCLUDED
 #define BUSQUEDAPP_H_INCLUDED
 
-
+int lSearch(int [],int,int); //lSearch(VectorD,tama�o,valor Buscado);
+void burbujaS(int [],int); //algoritmo de ordenamiento
+int binSearch(int [],int,int,int); //binSearch(vectorO,inicio,fin,vB)
+int indSearch(int[],int,int); //indSearch(VectorO,tama�o,vB);
+int intSearch(int [],int,int,int); //intSearch(vectorO,inicio,fin,vB);
 
 #endif // BUSQUEDAPP_H_INCLUDED
